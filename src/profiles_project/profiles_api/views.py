913b5d39db3b66ad07b3f8db775b812190ba1afe
singhsaurabh <gsaurabh.singh@gmail.com>
2017-12-12 00:10:39 +0530

    created serializer to our APIView.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
 
 # Create your views here.
 
 class HelloApiView(APIView):
     """Test API view."""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Return a list of APIView features."""
@@ -18,3 +23,16 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name."""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
